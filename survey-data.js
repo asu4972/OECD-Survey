923fb9aa5d49ed9fa910d28b15ed53c513a4fd91
asu4972 <69994692+asu4972@users.noreply.github.com>
2020-08-21 10:16:28 +1000

    Add files via upload

---
+++ survey-data.js
@@ -0,0 +1,34 @@
+
+
+
+var surveyDataObj = {
+
+	/*
+	CATEGORY NAMES---
+	skills
+	knowledge
+	attrAndVals
+
+	DOMAIN IDS-----
+	ce - Critical Enquiry
+	lg - Learning Growth
+	cr - Creativity
+	ag - Agency
+	rl - Rational Learning
+	mm - Making Meaning
+	ls - Learning Strategically
+	*/
+	
+	questions:[
+		{scoring:'negative', category:'skills', domainID:'ce',question:'Learning is primarily about remembering facts'},
+		{scoring:'positive', category:'skills', domainID:'ce',question:'I prefer it when I have to try really hard to understand something.'},
+
+		{scoring:'negative', category:'knowledge', domainID:'ce',question:'Teaching is primarily about precisely communicating curriculum content'},
+		{scoring:'positive', category:'knowledge', domainID:'ce',question:'I prefer it when I have to try really hard to understand my students'},
+
+		{scoring:'negative', category:'attrAndVals', domainID:'ce',question:'Life is primarily about precisely collecting knowledge and facts'},
+		{scoring:'positive', category:'attrAndVals', domainID:'ce',question:'I like looking for difficult issues in my community to try to understand'},
+	]
+};//end of data object
+
+
